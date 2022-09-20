8faccb8ee0721df89ac09937dc965272c86c11bb
Vineeth-07 <112814848+Vineeth-07@users.noreply.github.com>
2022-09-20 19:16:42 +0530

    Add files via upload

---
+++ L4/todo.js
@@ -0,0 +1,44 @@
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((todo) => todo.dueDate < new Date().toLocaleDateString("en-CA"));
+  };
+
+  const dueToday = () => {
+    return all.filter((todo) => todo.dueDate == new Date().toLocaleDateString("en-CA"));
+  };
+
+  const dueLater = () => {
+    return all.filter((todo) => todo.dueDate > new Date().toLocaleDateString("en-CA"));
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${todo.dueDate == new Date().toLocalDateString("en-CA") ? "" : todo.dueDate
+          }`
+      )
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+
+module.exports = todoList;
+
