6876886212247952c8ae1779faa3ebc6f2441168
Vineeth-07 <vineethdvs7@gmail.com>
2022-09-21 23:10:23 +0530

    Updated dates

--- L4/__tests__/todo.js
+++ L4/__tests__/todo.js
@@ -18,22 +18,27 @@ describe("Todolist Test Cases", () => {
   });
   test("Check for overdue", () => {
     expect(all.length).toBe(1);
+    const today = new Date();
+    const oneDay = 60 * 60 * 24 * 1000;
     add({
       title: "Complete Project",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA") - 1,
+      dueDate: new Date(today.getTime() - 1 * oneDay).toLocaleDateString(
+        "en-CA"
+      ),
     });
     expect(all.length).toBe(2);
     overdueItems = overdue();
-    expect(overdueItems.length).toBe(0);
+    expect(overdueItems.length).toBe(1);
   });
 
   test("Check for duetoday", () => {
     expect(all.length).toBe(2);
+    const today = new Date();
     add({
       title: "Go for shopping",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      dueDate: new Date(today.getTime()).toLocaleDateString("en-CA"),
     });
     expect(all.length).toBe(3);
     duetoday = dueToday();
@@ -42,10 +47,14 @@ describe("Todolist Test Cases", () => {
 
   test("Check for duelater", () => {
     expect(all.length).toBe(3);
+    const today = new Date();
+    const oneDay = 60 * 60 * 24 * 1000;
     add({
       title: "Having the exam",
       completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA") + 1,
+      dueDate: new Date(today.getTime() + 1 * oneDay).toLocaleDateString(
+        "en-CA"
+      ),
     });
     expect(all.length).toBe(4);
     duelater = dueLater();
